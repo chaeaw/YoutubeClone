9142caaba99b4c0ec6bd8d7f530e19db503c1ecc
chaeaw <chaeaw04@gmail.com>
2023-06-08 08:26:48 +0900

    [#004 update] morgan middlewware 추가
     morgan : HTTP request logger middleware, npm을 통해 설치

--- src/server.js
+++ src/server.js
@@ -1,33 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 5000;
+
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method}, ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    console.log("Not Allowed");
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middle!");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
+const home = (req, res) => {
+  return res.send("<h1>home</h1>");
 };
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", home);
 
 const handleListening = () => {
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);

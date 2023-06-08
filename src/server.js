efb7c949d91dfd11ad2f0fcb7c777797812790e9
chaeaw <chaeaw04@gmail.com>
2023-06-08 09:41:35 +0900

    [#005 update] router
    - router란? 클라이언트의 요청 경로(path)를 보고 이 요청을 처리해줄 수 있는 곳으로 기능을 전달해주는 역할
    - app.get("/", globerRouter) : 사용자가 정확하게 "/"루트를 get할 때에만 작동.
    - app.use("/", globerRouter) : 사용자가 "/"루트를 지나갈 때 작동. 즉, "/join"이나 "/login"을 get할 때에도 작동.
    - app.use는 일종의 중간 경로, "/users/edit" 에서 "/users"가 중간 경로인 것

--- src/server.js
+++ src/server.js
@@ -1,16 +1,39 @@
 import express from "express";
 import morgan from "morgan";
 
-const PORT = 5000;
+const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  return res.send("<h1>home</h1>");
-};
 app.use(logger);
-app.get("/", home);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => {
+  return res.send("Home");
+};
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => {
+  return res.send("Edit User");
+};
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => {
+  return res.send("Watch Video");
+};
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => {
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);

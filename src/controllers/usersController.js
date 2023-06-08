646c47b3ff30a6f2a3bf172edcc4c873050327d2
chaeaw <chaeaw04@gmail.com>
2023-06-08 10:34:06 +0900

    [#006 update] router, controller 폴더 정리
    - 프로젝트가 커지면 수많은 라우터와 컨트롤러가 필요해지므로 폴더로 나누어서 정리.
    - js파일은 각각의 모듈이므로 express가 필요할 경우 각각의 파일에서 import해야함. server.js에 import되어있는 것과는 무관!
    - export default는 해당 파일에서 지정된 한가지만을 내보냄 => 정확히 하나만 특정되어 있으므로 다른 파일에서 import할 때 다른 이름으로 들여와도 가능함.
    - export const ~ 또는 export { ... }를 사용할 경우에는 해당 파일에서 여러가지를 내보내므로 다른 파일에서 import할 때에 정확한 이름을 입력해서 불러와야 함!!

---
+++ src/controllers/usersController.js
@@ -0,0 +1,5 @@
+const join = (req, res) => res.send("Join");
+const edit = (req, res) => res.send("Edit");
+const deleteUser = (req, res) => res.send("Delete");
+
+export { join, edit, deleteUser };
---
+++ src/controllers/videoController.js
@@ -0,0 +1,5 @@
+const trending = (req, res) => res.send("HomePage Videos");
+const watch = (req, res) => res.send("Watch");
+const edit = (req, res) => res.send("Edit");
+
+export { trending, watch, edit };

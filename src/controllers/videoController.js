8e1b45ec7c309751bc7de1161f95fe3e8e7fd391
chaeaw04 <chaeaw04@gmail.com>
2023-06-16 00:02:50 +0900

    [#011 update] using ArrayData, form, POST method, express.urlencdoed -> req.body
    - 해당 arraydata는 임시로 만든 가짜 데이터!
    - id를 인덱스에 사용해, 해당 id를 가지고 있는 오브젝트를 필요한 컨트롤러에 불러옴.
    - 삼항 연산자 사용(watch.pug).
    - a(href="")에서, /(슬래시)가 붙으면 절대경로이고 없이 입력하면 상대경로.
    - form은 GET을 기본 메서드로, input의 name과 입력값을 주소에 입력해 새로 get함(검색창에서 사용).
    - GET : 접근, POST : 전송
    - routerName.route("루트").get(컨트롤러).post(컨트롤러) 와 같은 형태로 축약해서 작성할 수 있음.
    - req.body : form을 통해 submit된 데이터의 키-값쌍을 포함, 기본적으로 undefined이므로 express.urlencoded() 바디 파싱 미들웨어를 사용하여 값을 가져옴.
    - res.redirect("루트") : 해당루트로 다시 보내기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,41 +3,54 @@ const fakeUser = {
   loggedIn : false,
 }
 
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 38,
+    id: 1
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 65,
+    createdAt: "32 minutes ago",
+    views: 367,
+    id: 2
+  },
+  {
+    title: "Third Video",
+    rating: 3,
+    comments:1,
+    createdAt: "54 minutes ago",
+    views: 1,
+    id: 3
+  },
+]
+
 const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 38,
-      id: 1
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 65,
-      createdAt: "32 minutes ago",
-      views: 367,
-      id: 2
-    },
-    {
-      title: "Third Video",
-      rating: 3,
-      comments:1,
-      createdAt: "54 minutes ago",
-      views: 5,
-      id: 3
-    },
-  ]
   return res.render("home", { pageTitle: "Home", videos});
 };
-const see = (req, res) => {
-  return res.render("watch", { pageTitle: "Watch" });
+const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-const edit = (req, res) => {
-  return res.render("edit", { pageTitle: "Edit" });
+const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing:  ${video.title}`, video });
 };
+
+const postEdit = (req, res) => {
+  const {id} = req.params;
+  const { title} = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+
+}
 const search = (req, res) => res.send("Search");
 const deleteVideo = (req, res) => {
   console.log(req.params);
@@ -47,4 +60,4 @@ const upload = (req, res) => {
   return res.send("Upload Video");
 };
 
-export { trending, edit, search, see, deleteVideo, upload };
+export { trending, getEdit,postEdit, search, watch, deleteVideo, upload };

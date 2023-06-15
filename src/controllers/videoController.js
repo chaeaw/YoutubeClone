8a8855b611b0984cbd64bc6e7e353fe6e1552171
chaeaw04 <chaeaw04@gmail.com>
2023-06-15 21:25:02 +0900

    [#010 update] 배열 가져오기 및 mixins
    - 배열을 pug로 불러올 때는 itemName in arrayName, item.keyName
    - mixins는 include로 불러온 뒤 +mixinsName() 형식으로 사용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,33 @@ const fakeUser = {
 }
 
 const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", fakeUser });
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 38,
+      id: 1
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 65,
+      createdAt: "32 minutes ago",
+      views: 367,
+      id: 2
+    },
+    {
+      title: "Third Video",
+      rating: 3,
+      comments:1,
+      createdAt: "54 minutes ago",
+      views: 5,
+      id: 3
+    },
+  ]
+  return res.render("home", { pageTitle: "Home", videos});
 };
 const see = (req, res) => {
   return res.render("watch", { pageTitle: "Watch" });

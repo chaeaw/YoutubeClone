0dd64c2b0d821870f2d4303650d036ae934e0e8e
chaeaw04 <chaeaw04@gmail.com>
2023-06-20 13:51:53 +0900

    [#013 update] model.find(): promise 사용
    - 불러올 model들이 점점 늘어나면 server.js의 import가 복잡해지니 init.js(초기화)에 분리해놓음.
    - model.find() : 데이터 검색, 전에는 콜백함수도 가능했으나 mongoose의 현재 버전은 불가능해짐. 따라서 promise(비동기)를 사용해야 한다.
    - async(req, res) => { await model.find() } 같은 형식으로 작성.
    - promise 사용시 error를 잡기 위해선 try {} catch(error) {} 를 사용!

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,56 +1,25 @@
-const fakeUser = {
-  username : "user1",
-  loggedIn : false,
-}
+import Video from "../models/Video";
 
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 38,
-    id: 1
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 65,
-    createdAt: "32 minutes ago",
-    views: 367,
-    id: 2
-  },
-  {
-    title: "Third Video",
-    rating: 3,
-    comments:1,
-    createdAt: "54 minutes ago",
-    views: 1,
-    id: 3
-  },
-]
-
-const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos});
+const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos: [] });
 };
+
 const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
+
 const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing:  ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing:  ${video.title}` });
 };
 
 const postEdit = (req, res) => {
-  const {id} = req.params;
-  const { title} = req.body;
-  videos[id - 1].title = title;
+  const { id } = req.params;
+  const { title } = req.body;
   return res.redirect(`/videos/${id}`);
-
-}
+};
 const search = (req, res) => res.send("Search");
 const deleteVideo = (req, res) => {
   console.log(req.params);
@@ -60,4 +29,4 @@ const upload = (req, res) => {
   return res.send("Upload Video");
 };
 
-export { trending, getEdit,postEdit, search, watch, deleteVideo, upload };
+export { home, getEdit, postEdit, search, watch, deleteVideo, upload };

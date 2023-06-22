02c3ce9a2ad4088cc8c61d03497efe940174631f
chaeaw04 <chaeaw04@gmail.com>
2023-06-22 17:06:32 +0900

    [#016 update] schema 속성 추가, 이에 따른 input 수정
    - title, description에 minLength, maxLength 속성 추가 : minLength와 maxLength는 최소 글자수, 최대 글자수를 설정해줌.
    - input에서도 minLength, maxLength를 설정했는데 schema에서 또 해주는 이유는, 해킹 당했을 경우 html(input)이 수정가능해져 이 설정이 지워질 수 있으므로 데이터베이스에서도 추가적으로 설정.
    - title, description, hashtags에 trim 속성 추가 : trim은 앞 뒤 공백을 삭제해줌.

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, minlength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
-  // Date.now() 가 아닌 이유: function이 바로 실행되지않게 하기 위해! mongoose가 알아서 video를 새로 생성시켰을 때 실행시켜줌.
-  hashtags: [{ type: String }],
+  // Date.now() 가 아닌 이유: function이 바로 실행되지않게 하기 위해! video를 새로 생성시켰을 때 mongoose가 알아서 실행시켜줌.
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },

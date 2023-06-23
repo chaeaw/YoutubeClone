95a9f94c699462bd3d3c0c454f05f85bc66f1440
chaeaw04 <chaeaw04@gmail.com>
2023-06-23 12:11:04 +0900

    [#021 feat] video search 기능
    - model.sort({filter}) : 데이터들을 정렬. asc, desc 옵션들이 있음.
    - if문 안에서 밖에 있는 videos 배열을 채워주기 위해 const가 아닌(!!) let을 사용하고 if문 안에서도 const, let을 사용하지않고 바로 대입해줌.  if문 안에서 "const videos"로 작성한다면 그 안에서만 사용할 수 있게 되므로 주의!
    - form에서 get method로 들어온 정보를 가져오기 위해서는 req.query 사용! input의 name을 통해 값을 불러옴.(input이 여러 개일 경우도 가능)
    - 검색할 시 입력값과 일치하는게 아닌 입력값을 포함하는! 데이터들을 가져오기 위해서 정규표현식(reqular expression)사용.
    - $regex :  mongoDB에서 정규표현식을 사용하기 위해 쓰이는 키워드
    - ReqExp 객체 : 리터럴 표기법과 생성자로 생성 가능.
    - ("", "i") : i는 대소문자 구분을 없애줌. 이 외에도 여러가지가 있으니 찾아볼 것!

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "asc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -37,7 +37,6 @@ export const postEdit = async (req, res) => {
   });
   return res.redirect(`/videos/${id}`);
 };
-export const search = (req, res) => res.send("Search");
 
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
@@ -63,7 +62,19 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
   await Video.findByIdAndDelete(id, {});
   return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}`, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };

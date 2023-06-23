273a57f91c0478d48aa331b213962c11f4452721
chaeaw04 <chaeaw04@gmail.com>
2023-06-23 16:27:20 +0900

    [#022 feat] user join 기능 추가
    자잘한 수정 : globalRouter -> rootRouter, mongoose.connect 설정들 삭제(강의에선 오류로 인해 추가했지만 이 작업에선 오류가 없었으므로 삭제.)
    
    - userSchema 생성. join.pug 생성. getJoin 및 postJoin : model.create() 사용
    - bycrpt : 암호를 해시해주는 라이브러리
    ㄴ bcyrpt.hash(해시할 암호, saltRounds, function(err,hash){});
    ㄴ saltRounds : 해시 진행 횟수

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,21 @@
-const join = (req, res) => res.send("Join");
-const edit = (req, res) => res.send("Edit");
-const remove = (req, res) => res.send("Delete");
-const login = (req, res) => res.send("Login");
-const logout = (req, res) => res.send("Log Out");
-const see = (req, res) => res.send("See User");
+import User from "../models/User";
 
-export { join, edit, remove, login, logout, see };
+export const getJoin = (req, res) => {
+  return res.render("join", { pageTitle: "Join Account" });
+};
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
+export const edit = (req, res) => res.send("Edit");
+export const remove = (req, res) => res.send("Delete");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Log Out");
+export const see = (req, res) => res.send("See User");

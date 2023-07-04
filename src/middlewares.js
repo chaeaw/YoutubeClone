04231799d86522e4bc484dcdc7d0a4f2a47546bd
chaeaw04 <chaeaw04@gmail.com>
2023-07-04 14:01:00 +0900

    [#029 feat] login상태 확인용 미들웨어 추가
    현재 사이트를 방문한 유저의 로그인 상태에 따라 보여져야할 루트가 다르므로 이를 구분하기 위한 미들웨어 생성
    (버튼이 보이지 않아도 유저가 직접 url을 입력하면 이동하게 되므로 이를 방지하기위해!)
    - publicOnlyMiddleware : 비로그인(public)이 아니라면 되돌려보내기 ; 로그인 상태에서는 방문할 필요가 없는 루트들에게 적용.
    - protectorMiddleware : 로그인이 아니라면 되돌려보내기 ; 비로그인 상태에서는 방문할 필요가 없는 루트들에게 적용.

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,25 @@
 export const localsMiddlewares = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
 
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  // 로그인이 아니라면 되돌려보내기 => 로그인 상태에만 보여져야하는 루트에서 사용. ex) 프로필 수정 ...
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  // 로그인이라면 되돌려보내기 => 비로그인 상태에만 보여져야하는 루트에서 사용. ex) 로그인, 회원가입 ...
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,13 +7,24 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.route("/upload").get(getUpload).post(postUpload);
 // mongoose에서 랜덤으로 만들어주는 id가 24자의 16진수(0-9 a-f) 값이므로 정규식으로 일치하게끔 설정.
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-
-videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 export default videoRouter;
